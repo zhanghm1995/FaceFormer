5ffb044a78f631beba5f19c4d370a26aad3faccf
zhanghming <zhanghm_1995@qq.com>
2022-03-03 14:38:52 +0800

    update Face2D3DXFormer to support train 2d only

--- models/face_2d_3d_xfomer.py
+++ models/face_2d_3d_xfomer.py
@@ -7,48 +7,77 @@ Email: haimingzhang@link.cuhk.edu.cn
 Description: Face 2D-3D Cross-Modal transformer
 '''
 
-from xml.etree.ElementInclude import include
+from torch import Tensor
 import torch.nn as nn
 from torch.nn import TransformerEncoder, LayerNorm, TransformerEncoderLayer
 from .image_token_encoder import ImageTokenEncoder192
 
 
 class Face2D3DXFormer(nn.Module):
+    """The cross-modal transformer to fuse 2D and 3D face
+    """
     def __init__(self, config) -> None:
         super().__init__()
 
+        self.config = config
+
+        ## Define the transformer encoder
         encoder_layer = TransformerEncoderLayer(config['d_model'], config['nhead'], config['d_feed_forward'])
         encoder_norm = LayerNorm(config['d_model'])
         self.encoder = TransformerEncoder(encoder_layer, config['n_layer'], encoder_norm)
 
-        ## Define the 3D Decoder
-        self.face_3d_decoder = nn.Sequential(
-            nn.Conv1d(128, 256, kernel_size=3, stride=1,padding=1),
-            nn.BatchNorm1d(256),
-            nn.LeakyReLU(),
-            nn.Conv1d(256, 256, kernel_size=3, stride=1,padding=1),
-            nn.BatchNorm1d(256),
-            nn.Conv1d(256, 128, kernel_size=3, stride=1,padding=1),
-        )
+        if self.config['use_3d']:
+            ## Define the 3D Decoder
+            self.face_3d_decoder = nn.Sequential(
+                nn.Conv1d(128, 256, kernel_size=3, stride=1,padding=1),
+                nn.BatchNorm1d(256),
+                nn.LeakyReLU(),
+                nn.Conv1d(256, 256, kernel_size=3, stride=1,padding=1),
+                nn.BatchNorm1d(256),
+                nn.Conv1d(256, 128, kernel_size=3, stride=1,padding=1),
+            )
         
-        self.output_fc = nn.Linear(128, 64)
+            self.output_fc = nn.Linear(128, 64)
+        else:
+            print("[Face2D3DXFormer] Only use the 2D information")
 
         ## Define the 2D Decoder
         self.face_2d_decoder = ImageTokenEncoder192(in_ch=3)
     
-    def forward(self, x, masked_image):
-        x = self.encoder(x, mask=None, src_key_padding_mask=None) # (2S, B, E)
+    def forward_2d_only(self, input, masked_image: Tensor):
+        face_2d_embedding = self.encoder(input, mask=None, src_key_padding_mask=None) # (2S, B, E)
 
-        seq_len = int(x.shape[0] // 2)
-        face_3d_embedding = x[:seq_len, ...].permute(1, 2, 0) # to (B, E, S)
-        
-        face_3d_params = self.output_fc(self.face_3d_decoder(face_3d_embedding).permute(0, 2, 1)) # to (B, S, E)
-        
-        face_2d_embedding = x[seq_len:, ...] # (S, B, E)
         face_2d_embedding = face_2d_embedding.permute(1, 0, 2) # to (B, S, E)
 
         face_2d_image = self.face_2d_decoder(face_2d_embedding, masked_image)
+        return {'face_2d_image': face_2d_image}
 
-        return {'face_3d_params': face_3d_params,
-                'face_2d_image': face_2d_image}
+    def forward(self, x: Tensor, masked_image: Tensor):
+        """Forward the network
+
+        Args:
+            x (Tensor): (T, B, E)
+            masked_image (Tensor): (B, T, 3, H, W)
+
+        Returns:
+            _type_: _description_
+        """
+        if self.config['use_3d']:
+            x = self.encoder(x, mask=None, src_key_padding_mask=None) # (2S, B, E)
+
+            seq_len = int(x.shape[0] // 2)
+            face_3d_embedding = x[:seq_len, ...].permute(1, 2, 0) # to (B, E, S)
+            
+            face_3d_params = self.output_fc(self.face_3d_decoder(face_3d_embedding).permute(0, 2, 1)) # to (B, S, E)
+            
+            face_2d_embedding = x[seq_len:, ...] # (S, B, E)
+            face_2d_embedding = face_2d_embedding.permute(1, 0, 2) # to (B, S, E)
+
+            face_2d_image = self.face_2d_decoder(face_2d_embedding, masked_image)
+
+            return {'face_3d_params': face_3d_params,
+                    'face_2d_image': face_2d_image}
+        else:
+            output = self.forward_2d_only(x, masked_image)
+            return output
 
