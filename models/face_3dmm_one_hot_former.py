6f6f6abbc19141d1074c2c48d121718571b8ac89
zhanghming <zhanghm_1995@qq.com>
2022-04-01 23:09:10 +0800

    remove ALIBI for tgt_mask

--- models/face_3dmm_one_hot_former.py
+++ models/face_3dmm_one_hot_former.py
@@ -149,7 +149,9 @@ class Face3DMMOneHotFormer(nn.Module):
                     vertice_input = self.PPE(style_emb)
                 else:
                     vertice_input = self.PPE(vertice_emb)
-                tgt_mask = self.biased_mask[:, :vertice_input.shape[1], :vertice_input.shape[1]].clone().detach().to(device=device)
+                # tgt_mask = self.biased_mask[:, :vertice_input.shape[1], :vertice_input.shape[1]].clone().detach().to(device=device)
+                tgt_mask = None
+
                 memory_mask = enc_dec_mask(device, self.dataset, vertice_input.shape[1], hidden_states.shape[1])
                 vertice_out = self.transformer_decoder(vertice_input, hidden_states, tgt_mask=tgt_mask, memory_mask=memory_mask)
                 vertice_out = self.vertice_map_r(vertice_out)
